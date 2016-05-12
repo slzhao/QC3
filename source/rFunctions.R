ba743ef6342c4b34ebd80548c31581fac1076003
tdelhomme <delhommet@students.iarc.fr>
2016-05-12 12:01:50 +0200

    manage multiple SM with option -use_SM

--- source/rFunctions.R
+++ source/rFunctions.R
@@ -20,7 +20,9 @@ plot_dataFrame<-function(rawData,colBox=5:ncol(rawData),colList=NA,cex=1,dolog=F
 	} else {
 		row.names(rawData)<-sapply(temp,function(x) x[length(x)])
 	}
-  if (useSM==1) row.names(rawData)<-rawData[,"SM"]
+  if (useSM==1) {
+    row.names(rawData)<-make.unique(as.character(rawData[,"SM"]))
+  }
 	cex.axis<-c(cex,cex,cex-0.3,cex)
 	if(nobatch!=1){
 		for (x in colBox) {
